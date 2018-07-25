9984e9c33159d562d91776ec396587c35075d1fe
Gohar Attiq <Gohar Attiq@SPIDER>
2018-07-25 19:59:19 +0500

    Add project files.

---
+++ Games/Sanke.cpp
@@ -0,0 +1,51 @@
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+//using namespace std;
+//
+//
+//int width = 20, height = 20, dx = 10, dy = 10,fx=5,fy=5;      // Initialization
+//char player = 'O';
+////enum move = { STOP = 0,left,right,up,down };
+//
+//void Display();                             // Function Prototype
+//void Move();
+//
+//void main(bool p)
+//{
+//	Display();
+//	Move();
+//}
+//
+//
+//void Display()
+//{
+//	for (int i = 0; i < width; i++)
+//	{
+//		
+//		for (int j = 0; j < height; j++)
+//		{
+//			if (i == 0 || i == height-1)
+//			{
+//				cout << "*";
+//			}
+//			else if( j == 0 || j == width-1 )
+//			{
+//				cout << "*";
+//			}
+//			else
+//			{
+//				if (i == dy&&j == dx)
+//					cout << player;
+//				cout << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//}
+// 
+//
+//void Move()
+//{
+//
+//}
---
+++ Games/Source.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//#include <conio.h>
+//using namespace std;
+//void main()
+//{
+//	char ch;
+//	ch = _getche();
+//	ch = tolower(ch);
+//	cout << ch << endl;
+//}
